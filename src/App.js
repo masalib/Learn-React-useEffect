1d10e5c118796e2c6bd788bbdfb7722e3f252b7b
masanori <masalib@gmail.com>
2020-11-10 01:27:21 +0900

    render時にログやデータ取得処理 初期読み込みや対象が書き換わった時だけデータを取得する処理

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function App() {
   const [resourceType, setResourceType] = useState("posts");
+  const [items, setItems] = useState([]);
+  //すべてのdomが書き換わった時（render)に実行される
+  //useEffect(() => {
+  //  console.log("render");
+  //});
+
+  //書き換わらなくても実行？
+  console.log("render");
+
+  //初期読み込み時の場合
+  useEffect(() => {
+    console.log("onMount（初期読み込み時）");
+  }, []);
+
+  useEffect(() => {
+    console.log("render resourceType（対象が書き換わった）");
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}/`)
+      .then((response) => response.json())
+      .then((json) => setItems(json));
+  }, [resourceType]);
 
   return (
     <>
@@ -10,6 +30,9 @@ export default function App() {
       <button onClick={() => setResourceType("comments")}>comments</button>
 
       <h1>{resourceType}</h1>
+      {items.map((item) => {
+        return <pre>{JSON.stringify(item)}</pre>;
+      })}
     </>
   );
 }
